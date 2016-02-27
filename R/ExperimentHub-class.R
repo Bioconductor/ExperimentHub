112b629b5125d66cd60bad2bf0c99b3f670ac276
Martin Morgan <mtmorgan@fhcrc.org>
2016-02-27 19:52:32 +0000

    only install package if not already installed
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ExperimentHub@114068 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/ExperimentHub-class.R
+++ R/ExperimentHub-class.R
@@ -54,11 +54,10 @@ setMethod("[[", c("ExperimentHub", "numeric", "missing"),
     if (length(x[i]) != 1L)
         stop("'i' must be length 1")
     packagename <- AnnotationHub:::.count_resources(x[i], "preparerclass")
-    if (!eval(packagename) %in% search()) {
+    if (!packagename %in% rownames(installed.packages()))
         biocLite(packagename, suppressUpdates=TRUE)
-        message(paste0("see ?", packagename, " and browseVignettes('",
-                       packagename, "') for related documentation"))
-    }
+    message("see ?", packagename, " and browseVignettes('",
+            packagename, "') for documentation")
     callNextMethod(x, i, j, ...)
     ## or AnnotationHub:::.Hub_get1(x[i])
 })
