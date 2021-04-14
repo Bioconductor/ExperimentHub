f3c4ceb03373e3d9dc4e4bbd52f2cfa07f4ac443
Lori Shepherd <lori.shepherd@roswellpark.org>
2021-04-14 12:48:35 -0400

    change warning to .Deprecated

--- R/ExperimentHub-class.R
+++ R/ExperimentHub-class.R
@@ -19,9 +19,9 @@ ExperimentHub <-
     if ((cache == R_user_dir("ExperimentHub", which="cache")) && (Sys.getenv("EXPERIMENT_HUB_CACHE")=="")){
         olddefault = rappdirs::user_cache_dir(appname="ExperimentHub")
         if (dir.exists(olddefault) && (length(list.files(olddefault)) != 0)){
-            warning("DEPRECATION: As of ExperimentHub (>1.17.2), default caching location has changed.\n",
+            .Deprecated(msg=paste0("DEPRECATION: As of ExperimentHub (>1.17.2), default caching location has changed.\n",
                  "  Problematic cache: ", path.expand(olddefault), "\n",
-                 "  See https://bioconductor.org/packages/devel/bioc/vignettes/ExperimentHub/inst/doc/ExperimentHub.html#default-caching-location-update\n")
+                 "  See https://bioconductor.org/packages/devel/bioc/vignettes/ExperimentHub/inst/doc/ExperimentHub.html#default-caching-location-update\n"))
             cache = olddefault
         }
     }
